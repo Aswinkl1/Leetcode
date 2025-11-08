69f261c4c2c99ed7cfa93c9bb962a5770909bed5
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2025-11-08 10:42:02 +0530

    Time: 8 ms (88.99%), Space: 55.4 MB (90.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,19 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split("").sort().join("") === t.split("").sort().join("")
+   
+   if(s.length != t.length){
+    return false
+   }
+
+   let arr = new Array(26).fill(0)
+
+   for(let i=0;i<s.length;i++){
+    arr[s[i].charCodeAt(0) - 97] +=1
+    arr[t[i].charCodeAt(0) - 97] -=1
+
+   }
+
+   return arr.every(val=>val==0)
+
 };
