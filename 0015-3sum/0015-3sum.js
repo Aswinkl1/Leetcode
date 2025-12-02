1ffdecfb7cdf4ad2f0a220adc5602fc822789d20
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2025-12-02 10:43:04 +0530

    Time: 25 ms (98.66%), Space: 71.4 MB (90.94%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -6,7 +6,7 @@ var threeSum = function(nums) {
     nums.sort((a,b) => a-b)
     let result = []
     for(let i=0;i<nums.length;i++){
-        // if(nums[i]> 0 ) break
+        if(nums[i]> 0 ) break
         if(i > 0 && nums[i] === nums[i-1]) continue
 
         let l = i+1
