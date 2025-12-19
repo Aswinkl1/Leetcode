c9042c65eff76167cad8f64f61fea88ce24d3157
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2025-12-19 17:13:29 +0530

    Time: 0 ms (100%), Space: 54.8 MB (90%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies)
+    let ans = []
+    for(let i=0;i<candies.length;i++){
+        if(max <= candies[i] + extraCandies){
+            ans.push(true)
+        }else{
+            ans.push(false)
+        }
+    }
+    return ans
+};
