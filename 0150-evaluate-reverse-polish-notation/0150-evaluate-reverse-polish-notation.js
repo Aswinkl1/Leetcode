e4482e555834b9d6085476501a3f3af6e4add95c
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2025-12-31 10:01:22 +0530

    Time: 8 ms (33.28%), Space: 58.7 MB (39.28%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -5,13 +5,10 @@
 var evalRPN = function(tokens) {
     let stack = []
     let operator = {"+":1,"-":1,"/":1,"*":1}
-    
     for(let i of tokens){
         if(operator[i]){
             let op1 = stack.pop()
             let op2 = stack.pop()
-            console.log(op1)
-            console.log(op2)
             if(i == "+"){
                 stack.push(op2+op1)
                 
@@ -22,14 +19,12 @@ var evalRPN = function(tokens) {
                 stack.push(op2 * op1)
 
             }else if(i == "/"){
-                console.log("dv",op2 / op1)
+             
                 stack.push(Math.trunc(op2 / op1))
             }
-                console.log(stack)
         }else{
             stack.push(+i)
         }
     }
     return stack[0]
-    // console.log(stack)
 };
