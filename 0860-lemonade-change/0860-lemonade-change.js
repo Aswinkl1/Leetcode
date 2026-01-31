27150d5c0e981b0a6dda7572a9672f0e3774a193
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2026-01-31 18:58:17 +0530

    Time: 439 ms (5.02%), Space: 72.3 MB (5.23%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    
+    let balance = {5:0,10:0,20:0}
+    let lemonade = 5
+    for(let bill of bills){
+        let ret = bill - 5
+        if(ret !=0 && balance[ret]){
+            balance[ret] -= 1
+            balance[bill] += 1
+            console.log(balance)
+            continue
+        }
+
+        if(ret == 15){
+            if(balance[10]){
+                balance[10] -= 1
+                ret -= 10
+            }
+        }
+
+        if(ret <= balance[5] * 5){
+            while(balance[5] && ret !=0){
+                balance[5] -= 1
+                ret -= 5
+            }
+        console.log(bill,ret)
+        }
+        if(ret != 0){
+           return false
+        }
+        balance[bill] += 1
+        console.log(balance)
+    }
+
+    return true
+};
