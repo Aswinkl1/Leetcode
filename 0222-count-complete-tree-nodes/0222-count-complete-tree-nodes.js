3b9b28d45fad3fc8144d7f151235a3c04485f490
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2026-01-31 12:17:52 +0530

    Time: 2 ms (13.64%), Space: 74.4 MB (65.67%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    let count = 0
+
+    function inOrder(node){
+        if(!node) return 0
+        count++
+        // console.log(count)
+        inOrder(node.left)
+        inOrder(node.right)
+        
+
+    }
+    inOrder(root)
+    return count
+};
