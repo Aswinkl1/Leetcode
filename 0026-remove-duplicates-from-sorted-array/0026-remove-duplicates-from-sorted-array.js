ef2552ec927a11c8b4ec8d75646d6155dd07208f
Aswin <126509669+Aswinkl1@users.noreply.github.com>
2026-04-12 19:18:27 +0530

    Time: 1 ms (48.29%), Space: 57.1 MB (74.78%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,14 +3,24 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let seen = {}
-    for(let i=0;i<nums.length;i++){
-        // console.log(seen[nums[i]])
-        if(seen[nums[i]]){
-            nums.splice(i,1)
-            i--
+    // since it is sorted you dont need to use seen
+    // let seen = {}
+    // for(let i=0;i<nums.length;i++){
+    //     // console.log(seen[nums[i]])
+    //     if(seen[nums[i]]){
+    //         nums.splice(i,1)
+    //         i--
+    //     }
+    //         seen[nums[i]] = true
+    // }
+    // return nums.length
+    let k = 0
+    for(let i=1;i<nums.length;i++){
+        if(nums[k] !== nums[i]){
+            nums[k+1] = nums[i]
+            k++
         }
-            seen[nums[i]] = true
     }
-    return nums.length
+
+    return k +1
 };
